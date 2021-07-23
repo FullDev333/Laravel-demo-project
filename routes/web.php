3ac07c6cdbc9925660547759fb8320b44b5443a1
kimura-sang <istardev0103@gmail.com>
2021-07-24 01:32:30 +0900

    test

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/sample', function(){
+    echo "hello";
+});
 Route::get('/', function () {
     return view('welcome');
 });
